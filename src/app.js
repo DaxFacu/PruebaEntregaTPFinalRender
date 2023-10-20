4370785694a27fc8ebbfe1b7965f59b4182ef345
DaxFacu <113784822+DaxFacu@users.noreply.github.com>
2023-10-20 16:24:33 -0300

    first commit2

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ import { generateProducts } from "./utils/generateProducts.js";
 import errorHandler from "./middlewares/error.js";
 import { addLogger } from "./utils/logger.js";
 import swaggerJSDoc from "swagger-jsdoc";
-//import swaggerUiExpress from "swagger-Ui-Express";
+import swaggerUiExpress from "swagger-ui-express";
 import nodemailer from "nodemailer";
 
 const app = express();
